d1908fe0604cfd706684dcc51c7a88ce0feac9c0
sato0w0 <130539468+sato0w0@users.noreply.github.com>
2023-05-21 13:42:13 +0000

    [Add]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,47 @@
 class BooksController < ApplicationController
+before_action :is_matching_login_user, only: [:edit, :update]
+
   def new
   end
 
   def index
     @book = Book.new
     @books = Book.all
-    @user = User.all
+    @users = User.all
+    @user = current_user
   end
 
-  # 投稿データの保存
   def create
+    @user = current_user
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render "index"
+    end
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
+    @books = @user.books
+    @bookn = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render "edit"
+    end
   end
 
   def destroy
@@ -26,10 +50,20 @@ class BooksController < ApplicationController
     redirect_to '/books'
   end
 
-  # 投稿データのストロングパラメータ
   private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: "homes#top"
-  get 'homes/about', as: 'about'
-  resources :books, only: [:index, :show, :edit, :create, :destroy]
-  resources :users, only: [:index, :show, :edit]
+  get 'home/about' => 'homes#about', as: 'about'
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
